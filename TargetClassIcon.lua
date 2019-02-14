658776e24fac52c2ff6f32fa33f0d8e557890675
montinevra <montinevra@gmail.com>
2019-02-13 22:25:22 -0700

    first commit

---
+++ TargetClassIcon.lua
@@ -0,0 +1,31 @@
+local factionIconCoords = {
+	["Alliance"] = {.13,.47,.38,.72},
+	["Horde"] = {.53,.86,.39,.72}
+}
+
+TargetClassIconFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
+TargetClassIconFrame:RegisterEvent("UNIT_AURA")
+
+TargetClassIconFrame:SetScript("OnEvent", 
+	function (self, event, ...)
+		if event == "PLAYER_TARGET_CHANGED" or (event == "UNIT_AURA" and ... == "target" ) then
+			TargetType:SetText(UnitCreatureType("target"))
+		end
+
+		if event == "PLAYER_TARGET_CHANGED" then
+			local targetClass = select(2,UnitClass("target"))
+			local targetFaction = UnitFactionGroup("target")
+
+			if targetClass then 
+				TargetClassIcon:SetTexCoord(unpack(CLASS_ICON_TCOORDS[targetClass]))
+			end
+
+			if UnitIsPlayer("target") and targetFaction~="Neutral" then
+				TargetFaction:SetTexCoord(unpack(factionIconCoords[targetFaction]))
+				TargetFaction:Show()
+			else
+				TargetFaction:Hide()
+			end
+		end
+	end
+)
