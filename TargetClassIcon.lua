a024b6c6c3ccf4af23cc6a4dd04469215303671a
montinevra <montinevra@gmail.com>
2019-09-29 21:40:52 -0700

    add functionality to display spec

--- TargetClassIcon.lua
+++ TargetClassIcon.lua
@@ -4,8 +4,21 @@ local factionIconCoords = {
 }
 TargetClassIconFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
 TargetClassIconFrame:RegisterEvent("UNIT_AURA")
+TargetClassIconFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
 TargetClassIconFrame:SetScript("OnEvent", 
 	function (self, event, ...)
+		if event == "PLAYER_TARGET_CHANGED" or (event == "PLAYER_SPECIALIZATION_CHANGED" and ... == "target") then
+			TargetSpec:SetText("")
+			if CanInspect("target") then
+				NotifyInspect("target")
+				TargetClassIconFrame:RegisterEvent("INSPECT_READY")
+			end
+		end
+		if event == "INSPECT_READY" then
+			ClearInspectPlayer()
+			TargetSpec:SetText(select(2, GetSpecializationInfoByID(GetInspectSpecialization("target"))))
+			TargetClassIconFrame:UnregisterEvent("INSPECT_READY")
+		end
 		if event == "PLAYER_TARGET_CHANGED" or (event == "UNIT_AURA" and ... == "target" ) then
 			TargetType:SetText(UnitCreatureType("target"))
 		end
