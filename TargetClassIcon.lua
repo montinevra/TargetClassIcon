13a630fe398d3b254a664b3b7f7a1b133f00447d
Montinevra <montinevra@gmail.com>
2019-10-23 00:49:22 -0700

    move retail-exclusive code to separate file; faction banner anchors to class icon when spec is not available

--- TargetClassIcon.lua
+++ TargetClassIcon.lua
@@ -4,27 +4,8 @@ local factionIconCoords = {
 }
 TargetClassIconFrame:RegisterEvent("PLAYER_TARGET_CHANGED")
 TargetClassIconFrame:RegisterEvent("UNIT_AURA")
-TargetClassIconFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
 TargetClassIconFrame:SetScript("OnEvent", 
 	function (self, event, ...)
-		local spec_id
-		local spec_icon
-
-		if event == "PLAYER_TARGET_CHANGED" or (event == "PLAYER_SPECIALIZATION_CHANGED" and ... == "target") then
-			TargetSpecIcon:Hide()
-			if CanInspect("target") then
-				NotifyInspect("target")
-				TargetClassIconFrame:RegisterEvent("INSPECT_READY")
-			end
-		end
-		if event == "INSPECT_READY" then
-			spec_id = GetInspectSpecialization("target")
-			spec_icon = select(4, GetSpecializationInfoByID(spec_id))
-			TargetSpecIcon:SetTexture(spec_icon)
-			TargetSpecIcon:Show()
-			TargetClassIconFrame:UnregisterEvent("INSPECT_READY")
-			ClearInspectPlayer()
-		end
 		if event == "PLAYER_TARGET_CHANGED" or (event == "UNIT_AURA" and ... == "target" ) then
 			TargetType:SetText(UnitCreatureType("target"))
 		end
